37452e21241ef1e684a2f826027c757bc1560fee
Joao Goncalves <joao@joaogoncalves.net>
2025-05-14 12:07:22 +0100

    clicar mudar bookableindex

--- 2-hooks/src/components/Bookables.jsx
+++ 2-hooks/src/components/Bookables.jsx
@@ -1,23 +1,30 @@
+import { useState } from 'react';
 import {bookables} from '../db.json';
 
 export default function Bookables() {
+
+    const [bookableIndex, setBookableIndex] = useState(0);
 
     const group = "Rooms";
 
     const bookablesInGroup = bookables.filter( b => b.group === group);
 
-    
+    //let bookableIndex = 2; 
 
-    /* const items = bookablesInGroup.map( b => (
-        <li>{b.title}</li>
-    )) */
+    // clicar no li mudar o bookableindex
+    /* const changeBookable = (index) => {
+        bookableIndex = index;
+        console.log(bookableIndex);
+    } */
 
 
   return (
     <ul className='bookables'>
         {
-            bookablesInGroup.map( b => (
-                <li key={b.id}>
+            bookablesInGroup.map( (b, i) => (
+                <li key={b.id} 
+                    className={ i === bookableIndex ? "selected" : null}
+                    onClick={ () => setBookableIndex(i) }>
                     {b.title}
                 </li>
             ))
