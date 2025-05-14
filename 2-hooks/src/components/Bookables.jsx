cd6ef02d092d6ff4c6cbc23f0f929cdc370bbdf9
Joao Goncalves <joao@joaogoncalves.net>
2025-05-14 12:27:52 +0100

    next bookable index

--- 2-hooks/src/components/Bookables.jsx
+++ 2-hooks/src/components/Bookables.jsx
@@ -9,26 +9,36 @@ export default function Bookables() {
 
     const bookablesInGroup = bookables.filter( b => b.group === group);
 
-    //let bookableIndex = 2; 
+    const nextBookable = _ => {
+       // setBookableIndex( (bookableIndex + 1) % bookablesInGroup.length);
+       setBookableIndex ( index => (index +1) % bookablesInGroup.length);
+       // estou a delegar a responsabilidade de atualizacao do state ao react.
+    }
 
-    // clicar no li mudar o bookableindex
-    /* const changeBookable = (index) => {
-        bookableIndex = index;
-        console.log(bookableIndex);
-    } */
-
+    /* 0 / 4 = 0 resto 0
+    1 / 4 = 0 resto 1
+    ...
+    4 / 4 = 1  resto 0
+    5 / 4 = 1 /resto 1
+    6 / 4 = 1 /resto 2
+ */
 
   return (
-    <ul className='bookables'>
-        {
-            bookablesInGroup.map( (b, i) => (
-                <li key={b.id} 
-                    className={ i === bookableIndex ? "selected" : null}
-                    onClick={ () => setBookableIndex(i) }>
-                    {b.title}
-                </li>
-            ))
-        }
-    </ul>
+    <div>
+        <ul className='bookables'>
+            {
+                bookablesInGroup.map( (b, i) => (
+                    <li key={b.id} 
+                        className={ i === bookableIndex ? "selected" : null}
+                        onClick={ () => setBookableIndex(i) }>
+                        {b.title}
+                    </li>
+                ))
+            }
+        </ul>
+        <p>
+            <button autoFocus onClick={nextBookable}>Next</button>
+        </p>
+    </div>
   );
 }
