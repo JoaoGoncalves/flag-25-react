f29ac5b1d6a07d6261b9057a255e470f8f68e673
Joao Goncalves <joao@joaogoncalves.net>
2025-05-16 10:51:40 +0100

    final exemplo de useState, Bookables

--- 2-hooks/src/components/Bookables.jsx
+++ 2-hooks/src/components/Bookables.jsx
@@ -1,44 +1,79 @@
-import { useState } from 'react';
-import {bookables} from '../db.json';
+import { useState } from "react";
+import { bookables } from "../db.json";
 
 export default function Bookables() {
+  const [bookableIndex, setBookableIndex] = useState(0);
+  const [group, setGroup] = useState("Rooms");
+  const [hasDetails, setHasDetails] = useState(true);
+  //useReducer() , serve para retirar dos states todos um unico valor
 
-    const [bookableIndex, setBookableIndex] = useState(0);
+  const bookablesInGroup = bookables.filter((b) => b.group === group);
 
-    const group = "Rooms";
+  const bookable = bookablesInGroup[bookableIndex];
 
-    const bookablesInGroup = bookables.filter( b => b.group === group);
+  const groups = [...new Set(bookables.map((b) => b.group))];
 
-    const nextBookable = _ => {
-       // setBookableIndex( (bookableIndex + 1) % bookablesInGroup.length);
-       setBookableIndex ( index => (index +1) % bookablesInGroup.length);
-       // estou a delegar a responsabilidade de atualizacao do state ao react.
-    }
+  const nextBookable = (_) => {
+    // setBookableIndex( (bookableIndex + 1) % bookablesInGroup.length);
+    setBookableIndex((index) => (index + 1) % bookablesInGroup.length);
+    // estou a delegar a responsabilidade de atualizacao do state ao react.
+  };
 
-    /* 0 / 4 = 0 resto 0
-    1 / 4 = 0 resto 1
-    ...
-    4 / 4 = 1  resto 0
-    5 / 4 = 1 /resto 1
-    6 / 4 = 1 /resto 2
- */
+  const changeGroup = (e) => {
+    setBookableIndex(0);
+    setGroup(e.target.value);
+  }
 
   return (
-    <div>
-        <ul className='bookables'>
-            {
-                bookablesInGroup.map( (b, i) => (
-                    <li key={b.id} 
-                        className={ i === bookableIndex ? "selected" : null}
-                        onClick={ () => setBookableIndex(i) }>
-                        {b.title}
-                    </li>
-                ))
-            }
+    <>
+      <div>
+        <select value={group} onChange={changeGroup}>
+          {groups.map((g, i) => (
+            <option key={i}> {g} </option>
+          ))}
+        </select>
+        <ul className="bookables">
+          {bookablesInGroup.map((b, i) => (
+            <li
+              key={b.id}
+              className={i === bookableIndex ? "selected" : null}
+              onClick={() => setBookableIndex(i)}
+            >
+              {b.title}
+            </li>
+          ))}
         </ul>
         <p>
-            <button autoFocus onClick={nextBookable}>Next</button>
+          <button autoFocus onClick={nextBookable}>
+            Next
+          </button>
         </p>
-    </div>
+      </div>
+      <div>
+        {bookable && (
+          <>
+            <p>
+              <label htmlFor="details">Show Details</label>
+              <input
+                type="checkbox"
+                id="details"
+                checked={hasDetails}
+                onChange={(e) => setHasDetails(e.target.checked)}
+              />
+            </p>
+
+            {
+              //conditional rendering
+              hasDetails && (
+                <article>
+                  <h2> {bookable.title} </h2>
+                  <p> {bookable.notes}</p>
+                </article>
+              )
+            }
+          </>
+        )}
+      </div>
+    </>
   );
 }
