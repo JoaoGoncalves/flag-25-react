4e2d8bd923d1271b5f4cfc0ec2e799b996907d3f
Joao Goncalves <joao@joaogoncalves.net>
2025-05-16 12:13:49 +0100

    çampada com interuptor

--- _exercicios/lampada/src/App.jsx
+++ _exercicios/lampada/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import LightbulbSvg from './components/LightbulbSvg'
+import Interuptor from './components/Interuptor';
 
 function App() {
 
@@ -17,7 +18,8 @@ function App() {
       <hr />
       <LightbulbSvg fillColor={fillColor} />
       <br />
-      <button onClick={handleLight}> {light ? 'Off' : 'On'} </button>
+      <Interuptor ligarDesligar={handleLight} />
+      {/* <button onClick={handleLight}> {light ? 'Off' : 'On'} </button> */}
       <p> {light ? 'Ligado' : 'Desligado'} </p>
     </>
   )
---
+++ _exercicios/lampada/src/components/Interuptor.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Interuptor(props) {
+  return (
+    
+    <button onClick={props.ligarDesligar}> on/off </button>
+    
+  );
+}
