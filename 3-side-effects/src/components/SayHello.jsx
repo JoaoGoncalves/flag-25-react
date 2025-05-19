1870b692e9d7cc3869d5b0ea941a98fd5974f951
Joao Goncalves <joao@joaogoncalves.net>
2025-05-19 12:27:50 +0100

    useffect-inicio

---
+++ 3-side-effects/src/components/SayHello.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+export default function SayHello() {
+
+    const greetings = ['Olá' , 'Hola', 'Bonjour', 'Ciao', 'Good Morning'];
+
+    const [index, setIndex] = useState(0);
+
+
+    // use effect, utilizar sempre que tenha um side effect na componente
+    // executar sempre que o qualquer state for alterado
+    useEffect(
+        () => { 
+            document.title = greetings[index]
+        }
+    )
+
+
+    const updateGreetings = () => {
+        setIndex(Math.floor(Math.random() * greetings.length));
+    }
+
+  return (
+    <section>
+        <p> {greetings[index]} </p>
+        <button onClick={updateGreetings}> SAY HELLO </button>
+    </section>
+  );
+}
